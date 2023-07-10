d14e935f81d4683cf2156cdf1e5667f4203284a4
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-10 18:59:07 +0200

    Update config.py

--- controller/lockoff/config.py
+++ controller/lockoff/config.py
@@ -41,18 +41,23 @@ class Settings(BaseSettings):
     basic_auth_password: bytes = ""
     current_season: int = 2023
     prod: bool = False
-    tz: tzfile = gettz("Europe/Copenhagen")
+    tz_str: str = "Europe/Copenhagen"  # tzfile = gettz("Europe/Copenhagen")
 
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: Type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        env_settings: PydanticBaseSettingsSource,
-        dotenv_settings: PydanticBaseSettingsSource,
-        file_secret_settings: PydanticBaseSettingsSource,
-    ) -> Tuple[PydanticBaseSettingsSource, ...]:
-        return (MyCustomSource(settings_cls),)
+    def _get_tz(self):
+        return gettz(self.tz_str)
+
+    tz: tzfile = property(_get_tz)
+
+    # @classmethod
+    # def settings_customise_sources(
+    #     cls,
+    #     settings_cls: Type[BaseSettings],
+    #     init_settings: PydanticBaseSettingsSource,
+    #     env_settings: PydanticBaseSettingsSource,
+    #     dotenv_settings: PydanticBaseSettingsSource,
+    #     file_secret_settings: PydanticBaseSettingsSource,
+    # ) -> Tuple[PydanticBaseSettingsSource, ...]:
+    #     return (MyCustomSource(settings_cls),)
 
 
 settings = Settings()
