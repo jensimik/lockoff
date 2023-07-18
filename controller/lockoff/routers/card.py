21ad5f0d3f6f3fe097dfa0ec47dddb60870a2d8d
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-18 11:52:59 +0200

    Update card.py

--- controller/lockoff/routers/card.py
+++ controller/lockoff/routers/card.py
@@ -2,13 +2,13 @@ from typing import Annotated
 
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
 
-from ..depends import get_current_user_ids
+from ..depends import get_current_mobile
 
 router = APIRouter(tags=["card"])
 
 
 @router.get("/card.pdf")
 async def get_card_pdf(
-    current_user_ids: Annotated[list[int], Depends(get_current_user_ids)]
+    current_mobile: Annotated[list[int], Depends(get_current_mobile)]
 ):
-    return current_user_ids
+    return current_mobile
