75d42cc23530ea51d8bb392a5c31a161eb1671d6
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-18 21:21:23 +0200

    Update card.py

--- controller/lockoff/routers/card.py
+++ controller/lockoff/routers/card.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
 from tinydb import where
 
-from lockoff import get_current_mobile
+from lockoff import depends
 
 from ..config import settings
 from ..db import DB_member
@@ -12,10 +12,10 @@ router = APIRouter(tags=["card"])
 
 
 @router.get("/card.pdf")
-async def get_card_pdf(mobile: Annotated[str, Depends(get_current_mobile)]):
+async def get_card_pdf(mobile: Annotated[str, Depends(depends.get_current_mobile)]):
     return mobile
 
 
 @router.get("/card.pkpass")
-async def get_pkpass(mobile: Annotated[str, Depends(get_current_mobile)]):
+async def get_pkpass(mobile: Annotated[str, Depends(depends.get_current_mobile)]):
     return mobile
