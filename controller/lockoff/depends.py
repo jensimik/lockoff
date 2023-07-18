011b4017b36f9c4ddb5a82634e252fcde9a6c4d2
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-18 11:49:21 +0200

    /

--- controller/lockoff/depends.py
+++ controller/lockoff/depends.py
@@ -6,10 +6,10 @@ from jose import JWTError, jwt
 
 from .config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
-async def get_current_user_ids(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_mobile(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -17,9 +17,9 @@ async def get_current_user_ids(token: Annotated[str, Depends(oauth2_scheme)]):
     )
     try:
         payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
-        user_ids: list[int] = payload.get("sub")
-        if user_ids is None:
+        mobile: str = payload.get("sub")
+        if mobile is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    return user_ids
+    return mobile
