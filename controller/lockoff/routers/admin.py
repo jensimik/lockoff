3792fedf4f817b9c653a1be992905c07f31b7229
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-21 17:10:16 +0200

    Create admin.py

---
+++ controller/lockoff/routers/admin.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Annotated
+
+from dateutil.relativedelta import relativedelta
+from fastapi import APIRouter, HTTPException, Response, Security, status
+
+from lockoff import depends
+
+from ..access_token import TokenType, generate_access_token
+from ..apple_pass import ApplePass
+from ..config import settings
+from ..db import queries
+from ..depends import DBcon
+
+router = APIRouter(tags=["admin"])
+
+
+@router.get("/access_log")
+async def access_log(
+    mobile: Annotated[str, Security(depends.get_current_mobile, scopes=["admin"])]
+):
+    pass
