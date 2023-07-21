6ec06134457c96cd5b7d951f46065f5088351743
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-22 00:34:25 +0200

    Update admin.py

--- controller/lockoff/routers/admin.py
+++ controller/lockoff/routers/admin.py
@@ -31,18 +31,24 @@ async def access_log(
 
 
 @router.get("/members-without-danish-mobile")
-async def members_without_danish_mobile():
+async def members_without_danish_mobile(
+    mobile: Annotated[str, Security(depends.get_current_mobile, scopes=["admin"])],
+):
     pass
 
 
 @router.post("/send-membercard-by-email")
-async def send_membercard_by_email():
+async def send_membercard_by_email(
+    mobile: Annotated[str, Security(depends.get_current_mobile, scopes=["admin"])],
+):
     pass
     # send membercard to a member manually (to those with no danish mobile number in klubmodul?)
 
 
 @router.get("/system-status")
-async def system_status():
+async def system_status(
+    mobile: Annotated[str, Security(depends.get_current_mobile, scopes=["admin"])],
+):
     pass
     # return all alive?
     # last x access_log
