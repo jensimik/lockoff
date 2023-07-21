f2b452aa879e910fd6f973633822e97fb40e6db9
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-21 16:31:10 +0200

    Update depends.py

--- controller/lockoff/depends.py
+++ controller/lockoff/depends.py
@@ -12,7 +12,23 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 query_token = APIKeyQuery(name="token")
 
 
-async def get_current_mobile(token: str):
+# async def get_current_mobile(token: str):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     try:
+#         payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
+#         mobile: str = payload.get("sub")
+#         if mobile is None:
+#             raise credentials_exception
+#     except JWTError:
+#         raise credentials_exception
+#     return mobile
+
+
+async def get_current_mobile(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -44,19 +60,3 @@ async def get_db():
 
 
 DBcon = Annotated[aiosqlite.Connection, Depends(get_db)]
-
-
-# async def get_current_mobile(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
-#         mobile: str = payload.get("sub")
-#         if mobile is None:
-#             raise credentials_exception
-#     except JWTError:
-#         raise credentials_exception
-#     return mobile
