9c136d86e777e5316cc124363ea39bbd27cc93e7
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-21 15:00:41 +0200

    Update queries.sql

--- controller/lockoff/queries.sql
+++ controller/lockoff/queries.sql
@@ -22,7 +22,7 @@ select totp_secret
  where mobile = :mobile and active = true;
 
 
--- name: get_active_users_by_user_id_mobile
+-- name: get_active_users_by_user_id_mobile^
 -- get active users by mobile number
 select user_id, name, member_type, mobile, email, batch_id, active
   from users
