d195ae4e0c4c91c670139afe4d5a40097fa617c0
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-31 21:04:44 +0200

    Update paper_pass.py

--- controller/lockoff/card/paper_pass.py
+++ controller/lockoff/card/paper_pass.py
@@ -1,23 +1,23 @@
 # import base64
 # import io
 
-# import qrcode
+import qrcode
+from PIL import Image
+
 # from weasyprint import HTML
 
 # from .paper_template import template
-# from ..access_token import generate_dayticket_access_token
-# from PIL import Image
 
 
-# def generate_png(qr_code_data: str) -> Image:
-#     # generate qr code
-#     qr = qrcode.QRCode(
-#         version=None, error_correction=qrcode.constants.ERROR_CORRECT_M, border=0
-#     )
-#     qr.add_data(qr_code_data)
-#     qr.make(fit=True)
-#     img = qr.make_image(fill_color=(0, 0, 0), back_color=(255, 255, 255))
-#     return img
+def generate_png(qr_code_data: str) -> Image:
+    # generate qr code
+    qr = qrcode.QRCode(
+        version=None, error_correction=qrcode.constants.ERROR_CORRECT_M, border=0
+    )
+    qr.add_data(qr_code_data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=(0, 0, 0), back_color=(255, 255, 255))
+    return img
 
 
 # def generate_pdf(name: str, level: str, qr_code_data: str):
