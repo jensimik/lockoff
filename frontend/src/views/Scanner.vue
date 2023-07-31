546186e5bdab29ef2daf60702434667bc0f10e5d
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-31 23:27:06 +0200

    Update Scanner.vue

--- frontend/src/views/Scanner.vue
+++ frontend/src/views/Scanner.vue
@@ -1,20 +1,41 @@
 <script>
-import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 export default {
     name: "scanner",
     data() {
         return {
             code: "",
+            error: false,
         }
     },
     components: {
         QrcodeStream,
     },
     methods: {
-        onDetect (detectedCode) {
-            console.log(detectedCode);
-            this.code = detectedCode;
+        onDetect (detectedCodes) {
+            console.log(detectedCodes);
+            this.code = detectedCodes[0].rawValue;
+        },
+        paintOutline(detectedCodes, ctx) {
+            for (const detectedCode of detectedCodes) {
+                const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+                ctx.strokeStyle = 'red'
+                ctx.stroke = '3px'
+
+                ctx.beginPath()
+                ctx.moveTo(firstPoint.x, firstPoint.y)
+                for (const { x, y } of otherPoints) {
+                    ctx.lineTo(x, y)
+                }
+                ctx.lineTo(firstPoint.x, firstPoint.y)
+                ctx.closePath()
+                ctx.stroke()
+            }
+        },
+        logErrors(err) {
+            this.error = true;
         }
     }
 }
@@ -23,6 +44,7 @@ export default {
 
 
 <template>
-    <qrcode-stream @detect="onDetect"></qrcode-stream>
+    <qrcode-stream :track="paintOutline" @detect="onDetect" @error="logErrors"></qrcode-stream>
     <p>{{ code }}</p>
+    <p v-if="error">could not load camera somehow? are you on mobile?</p>
 </template>
