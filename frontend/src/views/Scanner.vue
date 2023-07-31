140ddbe4d266e9f1362c403beb0e79f2a18ce8bb
Jens Davidsen <jensimik@users.noreply.github.com>
2023-07-31 23:05:30 +0200

    .

---
+++ frontend/src/views/Scanner.vue
@@ -0,0 +1,21 @@
+<script>
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
+export default {
+    name: "scanner",
+    components: {
+        QrcodeStream,
+    },
+    methods: {
+        onDetect (detectedCode) {
+            console.log(detectedCode);
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <qrcode-stream @detect="onDetect"></qrcode-stream>
+</template>
