334a680475f12a1b6a132e65e8924447a0c2c48b
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-03 13:25:00 +0200

    .

--- controller/conftest.py
+++ controller/conftest.py
@@ -1,9 +1,34 @@
+from contextlib import asynccontextmanager
+
+import aiosqlite
 import pytest
-from lockoff.main import app
+from fakeredis import aioredis
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from fastapi_limiter import FastAPILimiter
+from lockoff.main import app
+from lockoff.misc import queries
+from lockoff.config import settings
+
+pytest.patch("redis_instance", aioredis.FakeRedis())
+
+
+@asynccontextmanager
+async def testing_lifespan(app: FastAPI):
+    _redis = aioredis.FakeRedis(encoding="utf-8", decode_responses=True)
+    await FastAPILimiter.init(_redis)
+    async with aiosqlite.connect(settings.db_file) as conn:
+        await queries.create_schema(conn)
+        await conn.commit()
+    yield
 
 
 @pytest.fixture
 def client() -> TestClient:
-    with TestClient(app=app, base_url="http://test") as client:
+    with TestClient(
+        app=app,
+        base_url="http://test",
+        lifespan=testing_lifespan,
+        shutdown=testing_lifespan,
+    ) as client:
         yield client
