c65383db5b47e39f2b0bb0ea2b9bf414cc85ca60
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-04 13:47:05 +0200

    Update test_reader.py

--- controller/tests/test_reader.py
+++ controller/tests/test_reader.py
@@ -32,7 +32,7 @@ async def test_dayticket(conn):
 
 
 @pytest.mark.asyncio
-async def test_reader(mocker, conn, mock_serial):
+async def test_reader_ok_token(mocker, conn, mock_serial):
     gfxdisplay = mocker.patch("lockoff.misc.GFXDisplay")
     buzz_in = mocker.patch("lockoff.reader.buzz_in")
     mocker.patch("aiosqlite.connect", lambda _: conn)
@@ -47,3 +47,24 @@ async def test_reader(mocker, conn, mock_serial):
 
     assert gfxdisplay.send_message.called_once_with(b"K")
     assert await buzz_in.called_once()
+
+
+@pytest.mark.asyncio
+async def test_reader_bad_token(mocker, conn, mock_serial):
+    gfxdisplay = mocker.patch("lockoff.misc.GFXDisplay")
+    buzz_in = mocker.patch("lockoff.reader.buzz_in")
+    mocker.patch("aiosqlite.connect", lambda _: conn)
+
+    ok_token = generate_access_token(user_id=1, token_type=TokenType.NORMAL)
+    bad_token = bytearray(ok_token)
+    bad_token[-1] = bad_token[-1] + 1
+    bad_token = bytes(bad_token)
+
+    stub = mock_serial.stub(
+        send_bytes=bad_token + b"\r",
+        receive_bytes=O_CMD.ERROR_SOUND + O_CMD.ERROR_LED,
+    )
+    await opticon_reader(display=gfxdisplay, run_infinite=False, url=mock_serial.port)
+
+    assert gfxdisplay.send_message.called_once_with(b"S")
+    assert await buzz_in.assert_not_called()
