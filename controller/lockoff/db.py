6652cf631f534c6d441c6cb3b0cf55aa04541d78
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-06 14:24:39 +0200

    .

---
+++ controller/lockoff/db.py
@@ -0,0 +1,31 @@
+from piccolo.engine.sqlite import SQLiteEngine
+from piccolo.table import Table
+from piccolo import columns
+
+from .config import settings
+from .access_token import TokenType
+
+DB = SQLiteEngine(path=settings.db_file)
+
+
+class Users(Table):
+    user_id = columns.Integer(primary_key=True)
+    name = columns.Varchar(length=100)
+    member_type = columns.Integer(choices=TokenType)
+    mobile = columns.Varchar(length=8)
+    email = columns.Varchar(length=80)
+    batch_id = columns.Varchar(length=25)
+    totp_secret = columns.Varchar(length=32)
+    active = columns.Boolean()
+
+
+class Dayticket(Table):
+    ticket_id = columns.Integer(primary_key=True)
+    batch_id = columns.Varchar(length=25)
+    expires = columns.Integer()
+
+
+class AccessLog(Table):
+    log_id = columns.Integer(primary_key=True)
+    obj_id = columns.Integer()
+    token_type = columns.Integer(choices=TokenType)
