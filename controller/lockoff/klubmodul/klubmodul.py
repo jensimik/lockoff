9fe41c377f004b7eb828e2240d430bf5f5e3ea35
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-06 22:29:14 +0200

    Update klubmodul.py

--- controller/lockoff/klubmodul/klubmodul.py
+++ controller/lockoff/klubmodul/klubmodul.py
@@ -315,53 +315,53 @@ async def refresh():
         batch_id = datetime.now(tz=settings.tz).isoformat(timespec="seconds")
         async with KMClient() as client, DB.transaction():
             # bulk upsert
-            print(
-                User.insert(
-                    *[
-                        User(
-                            id=user_id,
-                            name=name,
-                            token_type=member_type,
-                            email=simple_hash(email),
-                            mobile=simple_hash(mobile),
-                            batch_id=batch_id,
-                            totp_secret=pyotp.random_base32(),
-                            active=True,
-                        )
-                        async for user_id, name, member_type, email, mobile in client.get_members()
-                    ]
-                )
-            )
-            # await User.insert(
-            #     *[
-            #         User(
-            #             id=user_id,
-            #             name=name,
-            #             token_type=member_type,
-            #             email=simple_hash(email),
-            #             mobile=simple_hash(mobile),
-            #             batch_id=batch_id,
-            #             totp_secret=pyotp.random_base32(),
-            #             active=True,
-            #         )
-            #         async for user_id, name, member_type, email, mobile in client.get_members()
-            #     ]
-            # ).on_conflict(
-            #     target=User.id,
-            #     action="DO UPDATE",
-            #     values=[
-            #         User.name,
-            #         User.email,
-            #         User.mobile,
-            #         User.batch_id,
-            #         User.active,
-            #     ],
+            # print(
+            #     User.insert(
+            #         *[
+            #             User(
+            #                 id=user_id,
+            #                 name=name,
+            #                 token_type=member_type,
+            #                 email=simple_hash(email),
+            #                 mobile=simple_hash(mobile),
+            #                 batch_id=batch_id,
+            #                 totp_secret=pyotp.random_base32(),
+            #                 active=True,
+            #             )
+            #             async for user_id, name, member_type, email, mobile in client.get_members()
+            #         ]
+            #     )
             # )
-            # # mark old data as inactive
-            # try:
-            #     await User.update({User.active: False}).where(User.batch_id != batch_id)
-            # except Exception as ex:
-            #     log.exception("failed on update?")
+            await User.insert(
+                *[
+                    User(
+                        id=user_id,
+                        name=name,
+                        token_type=member_type,
+                        email=simple_hash(email),
+                        mobile=simple_hash(mobile),
+                        batch_id=batch_id,
+                        totp_secret=pyotp.random_base32(),
+                        active=True,
+                    )
+                    async for user_id, name, member_type, email, mobile in client.get_members()
+                ]
+            ).on_conflict(
+                target=User.id,
+                action="DO UPDATE",
+                values=[
+                    User.name,
+                    User.email,
+                    User.mobile,
+                    User.batch_id,
+                    User.active,
+                ],
+            )
+            # mark old data as inactive
+            try:
+                await User.update({User.active: False}).where(User.batch_id != batch_id)
+            except Exception as ex:
+                log.exception("failed on update?")
 
 
 async def klubmodul_runner(one_time_run: bool = False):
