8b38f72af68d4b39b109784b1aa55adc27691b2b
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-09 02:17:27 +0200

    Update apple_pass.py

--- controller/lockoff/card/apple_pass.py
+++ controller/lockoff/card/apple_pass.py
@@ -69,7 +69,9 @@ class AppleNotifier:
                 "apns-expiration": int(time.time()) + 3600,
                 "apns-push-type": "background",
             }
-            response = await self.client.post(f"/3/device/{push_token}", json={})
+            response = await self.client.post(
+                f"/3/device/{push_token}", headers=headers, json={}
+            )
             log.info(response.status_code)
             log.info(response.json())
             log.info(response.text)
