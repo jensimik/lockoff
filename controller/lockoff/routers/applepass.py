29a604579f8bb925f2d55cb8a0a8619c76c1e544
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-09 23:51:26 +0200

    Update applepass.py

--- controller/lockoff/routers/applepass.py
+++ controller/lockoff/routers/applepass.py
@@ -95,6 +95,7 @@ async def get_list_of_updateable_passes_to_device(
         APReg.serial_number.join_on(APPass.id).update_tag.as_alias("update_tag"),
     ).where(APReg.device_library_identifier == device_library_identifier)
     if passesUpdatedSince:
+        log.info("doing passesupdatesince query")
         query = query.where(WhereRaw("update_tag > {}", passesUpdatedSince))
     data = await query
     log.info(data)
