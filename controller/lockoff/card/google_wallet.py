aee337522a6fc3d65e86da7f4f52783f7e779ca1
Jens <jensimik@users.noreply.github.com>
2023-08-13 23:03:05 +0200

    Google wallet part1 (#120)

---
+++ controller/lockoff/card/google_wallet.py
@@ -0,0 +1,87 @@
+import json
+import os
+import typing
+import uuid
+from types import TracebackType
+
+import httpx
+from google.auth import crypt, jwt
+from google.oauth2.service_account import Credentials
+
+from ..config import settings
+
+U = typing.TypeVar("U", bound="GooglePass")
+
+
+class GoogleAuth(httpx.Auth):
+    requires_response_body = True
+
+    def __init__(self):
+        self.credentials = Credentials.from_service_account_file(
+            filename=settings.google_service_account,
+            scopes=["https://www.googleapis.com/auth/wallet_object.issuer"],
+        )
+        self.credentials._always_use_jwt_access = True
+        self.credentials._create_self_signed_jwt()
+
+    def auth_flow(self, request: httpx.Request):
+        if self.credentials.expired:
+            self.credentials._jwt_credentials.refresh()
+        self.credentials._jwt_credentials.apply(request.headers)
+        yield request
+
+
+class GooglePass:
+    def __init__(self):
+        self.signer = crypt.RSASigner.from_service_account_file(
+            filename=settings.google_service_account
+        )
+        self.issuer_id = settings.google_issuer_id
+        self.base_url = "https://walletobjects.googleapis.com/walletobjects/v1"
+        self.object_url = "/genericObject"
+
+        auth = GoogleAuth(credentials=self.service_account)
+
+        self.client = httpx.AsyncClient(
+            auth=auth,
+            base_url=self.base_url,
+        )
+
+    async def expire_pass(self, pass_id: str):
+        url = f"/genericObject/{self.issuer_id}.{pass_id}"
+        patch_body = {"state": "EXPIRED"}
+        response = await self.client.patch(url, json=patch_body)
+        return response.status_code == 200
+
+    async def patch_pass(self, pass_id: str, patch: dict):
+        url = f"/genericObject/{self.issuer_id}.{pass_id}"
+        response = await self.client.patch(url, json=patch)
+        return response.status_code == 200
+
+    def _generate_class(self):
+        pass
+
+    def create_pass(self, pass_id: str):
+        claims = {
+            "iss": self.credentials.service_account_email,
+            "aud": "google",
+            "origins": ["lockoff.nkk.dk"],
+            "typ": "savetowallet",
+            "payload": {
+                "genericClasses": [self._generate_generic_class()],
+                "genericObjects": [self._generate_generic_object(pass_id=pass_id)],
+            },
+        }
+        token = jwt.encode(self.signer, claims).decode("utf-8")
+        return f"https://pay.google.com/gp/v/save/{token}"
+
+    async def __aenter__(self: U) -> U:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: typing.Optional[typing.Type[BaseException]] = None,
+        exc_value: typing.Optional[BaseException] = None,
+        traceback: typing.Optional[TracebackType] = None,
+    ) -> None:
+        await self.client.aclose()
