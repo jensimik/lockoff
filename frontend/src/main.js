933e70fbad41139bb89845d7aebd1063dec3d2cd
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-13 13:30:32 +0200

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
+import * as Sentry from "@sentry/vue";
 import { createApp } from 'vue';
 import "../node_modules/picnic/picnic.min.css";
 import App from './App.vue';
 import router from './router';
 import './style.css';
-import * as Sentry from "@sentry/vue";
 
+const app = createApp(App);
 
 Sentry.init({
-    Vue,
+    app,
     dsn: "https://642cb9b1809b78966c6047f962af4aad@o4504589908377600.ingest.sentry.io/4505697343373312",
     integrations: [
       new Sentry.BrowserTracing({
@@ -23,8 +24,6 @@ Sentry.init({
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
-
-const app = createApp(App);
 
 app.use(router);
 
