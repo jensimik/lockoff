2e5e54e67077a9e998cefb78e08aa417f5463c69
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-13 13:38:07 +0200

    Update Card.vue

--- frontend/src/views/Card.vue
+++ frontend/src/views/Card.vue
@@ -49,22 +49,22 @@ export default {
               <a class="button" target="_blank" :href="baseURL + '/' + user.token + '/membership-card.pkpass'">📱 mobile pass<span v-if="os== 'Android'">*</span></a>
             </div>
             <a v-if="os == 'iOS'" target="_blank" :href="baseURL + '/' + user.token + '/membership-card.pkpass'"><img class="addtoapplewallet" src="/US-UK_Add_to_Apple_Wallet_RGB_101421.svg" /></a>
+            <div v-if="os == 'Android'">
+              <p>* to use the digital wallet on android phones you need an app - i recommend one of these apps:</p>
+              <div class="flex two">
+                <div>
+                  <h4>WalletPass</h4>
+                  <a target="_blank" :href="'https://walletpass.io?u=' + encodeURIComponent(baseURL + '/' + user.token + '/membership-card.pkpass')"><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+                </div>
+                <div>
+                  <h4>PassWallet</h4>
+                  <a target="_blank" :href="'https://passwallet.page.link/?apn=com.attidomobile.passwallet&link=' + encodeURIComponent(baseURL + '/' + user.token + '/membership-card.pkpass')"><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+                </div>
+              </div>
+            </div>
           </footer>
         </article>
-        </div>
-        <div v-if="os == 'Android'">
-          <p>* to use the digital wallet on android phones you need an app - i recommend one of these apps:</p>
-          <div class="flex two">
-            <div>
-              <h4>WalletPass</h4>
-              <a target="_blank" :href="'https://walletpass.io?u=' + encodeURIComponent(baseURL + '/' + user.token + '/membership-card.pkpass')"><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
-            </div>
-            <div>
-              <h4>PassWallet</h4>
-              <a target="_blank" :href="'https://passwallet.page.link/?apn=com.attidomobile.passwallet&link=' + encodeURIComponent(baseURL + '/' + user.token + '/membership-card.pkpass')"><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
-            </div>
-          </div>
-        </div>
+      </div>
     </div>
 </template>
 
