ecec0b2c377fdd5217d5c8fb23f17d0898cfb946
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-14 11:59:08 +0200

    Update google_wallet.py

--- controller/lockoff/card/google_wallet.py
+++ controller/lockoff/card/google_wallet.py
@@ -24,11 +24,9 @@ class GoogleAuth(httpx.Auth):
         self.credentials._create_self_signed_jwt(audience=None)
 
     def auth_flow(self, request: httpx.Request):
-        if self.credentials._jwt_credentials.expired:
-            self.credentials._jwt_credentials.refresh()
-        self.credentials._jwt_credentials.apply(
-            request.headers, self.credentials._jwt_credentials.token.decode()
-        )
+        if self.credentials.expired:
+            self.credentials.refresh()
+        self.credentials.apply(request.headers, self.credentials.token)
         yield request
 
 
