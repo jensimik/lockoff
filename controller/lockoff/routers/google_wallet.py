00f7efac7cef3b0fd9bd67eb83f08e1c05771aa2
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-14 22:29:12 +0200

    Update google_wallet.py

--- controller/lockoff/routers/google_wallet.py
+++ controller/lockoff/routers/google_wallet.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 async def callback(request: Request):
     # TODO: verify signature!
     # https://notificare.com/blog/2022/07/08/Handle-Google-Wallet-callbacks-with-nodejs/
-    data = request.json()
+    data = await request.json()
     event = json.loads(data["signedMessage"])
     _, pass_id = event["objectId"].split(".")
     event_type = event["eventType"]
