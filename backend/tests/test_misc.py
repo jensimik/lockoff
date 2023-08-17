d79129525da992a4124b0f1ab17ef1af863647b9
Jens Davidsen <jensimik@users.noreply.github.com>
2023-08-17 13:21:35 +0200

    .

--- backend/tests/test_misc.py
+++ backend/tests/test_misc.py
@@ -2,36 +2,36 @@ import pytest
 import asyncio
 from datetime import datetime
 from lockoff.config import settings
-from lockoff.misc import GFXDisplay, simple_hash, buzz_in
+from lockoff.misc import simple_hash
 
 
-@pytest.mark.asyncio
-async def test_display(mock_serial):
-    lcd = GFXDisplay()
-    await lcd.setup(url=mock_serial.port)
+# @pytest.mark.asyncio
+# async def test_display(mock_serial):
+#     lcd = GFXDisplay()
+#     await lcd.setup(url=mock_serial.port)
 
-    # test send message
-    test_message = b"K"
-    stub = mock_serial.stub(
-        send_bytes=b"",
-        receive_bytes=test_message,
-    )
-    await lcd.send_message(test_message)
-    await asyncio.sleep(0.1)
+#     # test send message
+#     test_message = b"K"
+#     stub = mock_serial.stub(
+#         send_bytes=b"",
+#         receive_bytes=test_message,
+#     )
+#     await lcd.send_message(test_message)
+#     await asyncio.sleep(0.1)
 
-    assert stub.called
-    assert stub.calls == 1
+#     assert stub.called
+#     assert stub.calls == 1
 
-    now = datetime.now(tz=settings.tz)
-    stub = mock_serial.stub(
-        send_bytes=b"",
-        receive_bytes=b"," if now.hour < 7 else b".",
-    )
-    await lcd.runner(one_time_run=True)
+#     now = datetime.now(tz=settings.tz)
+#     stub = mock_serial.stub(
+#         send_bytes=b"",
+#         receive_bytes=b"," if now.hour < 7 else b".",
+#     )
+#     await lcd.runner(one_time_run=True)
 
-    await asyncio.sleep(0.1)
-    assert stub.called
-    assert stub.calls == 1
+#     await asyncio.sleep(0.1)
+#     assert stub.called
+#     assert stub.calls == 1
 
 
 def test_simple_hash():
@@ -44,9 +44,9 @@ def test_simple_hash():
     assert hash1 == hash1_again
 
 
-@pytest.mark.asyncio
-async def test_buzz_in(mocker):
-    relay = mocker.patch("lockoff.misc.relay")
-    await buzz_in(sleep=0)
-    relay.on.assert_called_once()
-    relay.off.assert_called_once()
+# @pytest.mark.asyncio
+# async def test_buzz_in(mocker):
+#     relay = mocker.patch("lockoff.misc.relay")
+#     await buzz_in(sleep=0)
+#     relay.on.assert_called_once()
+#     relay.off.assert_called_once()
--- backend/tests/test_reader.py
+++ backend/tests/test_reader.py
@@ -52,62 +52,62 @@ async def test_dayticket():
     await check_dayticket(user_id=0)
 
 
-@pytest.mark.asyncio
-async def na_test_reader_ok_token(mocker, mock_serial):
-    send_message = mocker.patch("lockoff.misc.GFXDisplay.send_message")
-    buzz_in = mocker.patch("lockoff.reader.buzz_in")
-    ok_token = generate_access_token(user_id=1, token_type=TokenType.NORMAL)
+# @pytest.mark.asyncio
+# async def na_test_reader_ok_token(mocker, mock_serial):
+#     send_message = mocker.patch("lockoff.misc.GFXDisplay.send_message")
+#     buzz_in = mocker.patch("lockoff.reader.buzz_in")
+#     ok_token = generate_access_token(user_id=1, token_type=TokenType.NORMAL)
 
-    async def fake_readuntil(*args, **kwargs):
-        return ok_token + b"\r"
+#     async def fake_readuntil(*args, **kwargs):
+#         return ok_token + b"\r"
 
-    mocker.patch("asyncio.StreamReader.readuntil", fake_readuntil)
-    stream_writer = mocker.patch("asyncio.StreamWriter.write")
-    display = GFXDisplay()
+#     mocker.patch("asyncio.StreamReader.readuntil", fake_readuntil)
+#     stream_writer = mocker.patch("asyncio.StreamWriter.write")
+#     display = GFXDisplay()
 
-    reader = Reader()
-    await reader.setup(display=display, url=mock_serial.port)
+#     reader = Reader()
+#     await reader.setup(display=display, url=mock_serial.port)
 
-    await reader.runner(one_time_run=True)
+#     await reader.runner(one_time_run=True)
 
-    # sleep a little to allow background tasks to callback
-    await asyncio.sleep(0.1)
+#     # sleep a little to allow background tasks to callback
+#     await asyncio.sleep(0.1)
 
-    stream_writer.assert_any_call(O_CMD.OK_SOUND)
-    stream_writer.assert_any_call(O_CMD.OK_LED)
+#     stream_writer.assert_any_call(O_CMD.OK_SOUND)
+#     stream_writer.assert_any_call(O_CMD.OK_LED)
 
-    send_message.assert_awaited_once_with(b"K")
-    buzz_in.assert_awaited_once()
+#     send_message.assert_awaited_once_with(b"K")
+#     buzz_in.assert_awaited_once()
 
 
-@pytest.mark.asyncio
-async def na_test_reader_bad_token(mocker, mock_serial):
-    send_message = mocker.patch("lockoff.misc.GFXDisplay.send_message")
-    buzz_in = mocker.patch("lockoff.reader.buzz_in")
-    ok_token = generate_access_token(user_id=1, token_type=TokenType.NORMAL)
-    bad_token = bytearray(ok_token)
-    bad_token[-1] = bad_token[-4]
-    bad_token[-2] = bad_token[-5]
-    bad_token[-3] = bad_token[-6]
-    bad_token = bytes(bad_token)
+# @pytest.mark.asyncio
+# async def na_test_reader_bad_token(mocker, mock_serial):
+#     send_message = mocker.patch("lockoff.misc.GFXDisplay.send_message")
+#     buzz_in = mocker.patch("lockoff.reader.buzz_in")
+#     ok_token = generate_access_token(user_id=1, token_type=TokenType.NORMAL)
+#     bad_token = bytearray(ok_token)
+#     bad_token[-1] = bad_token[-4]
+#     bad_token[-2] = bad_token[-5]
+#     bad_token[-3] = bad_token[-6]
+#     bad_token = bytes(bad_token)
 
-    async def fake_readuntil(*args, **kwargs):
-        return bad_token + b"\r"
+#     async def fake_readuntil(*args, **kwargs):
+#         return bad_token + b"\r"
 
-    mocker.patch("asyncio.StreamReader.readuntil", fake_readuntil)
-    stream_writer = mocker.patch("asyncio.StreamWriter.write")
-    display = GFXDisplay()
+#     mocker.patch("asyncio.StreamReader.readuntil", fake_readuntil)
+#     stream_writer = mocker.patch("asyncio.StreamWriter.write")
+#     display = GFXDisplay()
 
-    reader = Reader()
-    await reader.setup(display=display, url=mock_serial.port)
+#     reader = Reader()
+#     await reader.setup(display=display, url=mock_serial.port)
 
-    await reader.runner(one_time_run=True)
+#     await reader.runner(one_time_run=True)
 
-    # sleep a little to allow background tasks to callback
-    await asyncio.sleep(0.1)
+#     # sleep a little to allow background tasks to callback
+#     await asyncio.sleep(0.1)
 
-    stream_writer.assert_any_call(O_CMD.ERROR_SOUND)
-    stream_writer.assert_any_call(O_CMD.ERROR_LED)
+#     stream_writer.assert_any_call(O_CMD.ERROR_SOUND)
+#     stream_writer.assert_any_call(O_CMD.ERROR_LED)
 
-    send_message.assert_awaited_once_with(b"S")
-    buzz_in.assert_not_awaited()
+#     send_message.assert_awaited_once_with(b"S")
+#     buzz_in.assert_not_awaited()
